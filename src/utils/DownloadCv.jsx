6b1e89e0ee09e616e1b6004d6848a44d80c83d8c
Akmalov Dilmurod <dilmurodakmalov20@gmail.com>
2025-11-18 22:03:08 +0500

    fixing small issues

--- src/utils/DownloadCv.jsx
+++ src/utils/DownloadCv.jsx
@@ -1,12 +1,12 @@
-export default function DownloadCv() {
-    const handleDownload = () => {
-        const link = document.createElement("a");
-        link.href = "/cv.pdf";
-        link.download = "My-CV.pdf";
-        link.click();
-    }
+// export default function DownloadCv() {
+//     const handleDownload = () => {
+//         const link = document.createElement("a");
+//         link.href = "/cv.pdf";
+//         link.download = "My-CV.pdf";
+//         link.click();
+//     }
 
-    return (
+//     return (
         
-    )
-}
+//     )
+// }
